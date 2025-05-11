b9bf89c21175bf362070493f89e5255ad2a2a8a3
Alexandr Kite <alexandr.kite@tutamail.com>
2025-05-11 13:42:57 +0300

    1

--- middleware.ts
+++ middleware.ts
@@ -1,46 +1,4 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { getToken } from "next-auth/jwt";
 
-// const secret = process.env.NEXTAUTH_SECRET;
-
-// export async function middleware(req: NextRequest) {
-//   const token = await getToken({ req, secret });
-//   const { pathname } = req.nextUrl;
-
-//   // Защищённые маршруты (только для авторизованных)
-//   const protectedPaths = ["/blog", "/profile", "/dashboard"];
-
-//   // Публичные маршруты, недоступные авторизованным
-//   const authRestrictedPaths = ["/login", "/signup"];
-
-//   // Проверка защищённых маршрутов
-//   if (protectedPaths.some((path) => pathname.startsWith(path))) {
-//     if (!token) {
-//       const loginUrl = new URL("/login", req.url);
-//       loginUrl.searchParams.set("callbackUrl", pathname);
-//       return NextResponse.redirect(loginUrl);
-//     }
-//   }
-
-//   // Перенаправление авторизованных с /login и /signup
-//   if (authRestrictedPaths.includes(pathname)) {
-//     if (token) {
-//       return NextResponse.redirect(new URL("/", req.url));
-//     }
-//   }
-
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: [
-//     "/blog/:path*",
-//     "/profile/:path*",
-//     "/dashboard/:path*",
-//     "/login",
-//     "/signup",
-//   ],
-// };
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
